19add457cf37c9e24f80ec91b936c0970c710deb
zcg1011713962 <56151248+zcg1011713962@users.noreply.github.com>
2023-07-08 23:02:26 +0800

    服务发现

--- chatgpt-api/src/main/java/com/chat/cst/APIConstant.java
+++ chatgpt-api/src/main/java/com/chat/cst/APIConstant.java
@@ -4,9 +4,7 @@ public class APIConstant {
     public static final double DEFAULT_RATE_LIMITER = 10.0;
     public static final String CHAT = "/v1/chat/completions";
     public static final String CHAT_COUNT = "/v1/chat/count";
-    public static final String IMAGES_GENERATIONS = "/v1/images/generations";
-
-
+    public static final String IMAGES_GENERATIONS = "/v1/chat/images/generations";
 
 
 }
